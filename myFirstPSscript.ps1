41c30f76921f3f6ca75e9c53901f49cb3d342a8e
Tania Haiuk <t.v.gayuk@gmail.com>
2022-01-21 14:51:13 +0200

    resources and scripts folders created

---
+++ myFirstPSscript.ps1
@@ -0,0 +1,40 @@
+﻿# start of script
+
+# Set-ExecutionPolicy RemoteSigned
+
+#function defined in script
+
+function Get-Power([int]$x, [int]$y)
+{
+    if ($y -gt 0) {
+        return $x * (Get-Power $x (--$y)) 
+    }
+
+    else { return 1 }
+}
+
+function Get-PSVersion{
+    $PSVersionTable.PSVersion
+}
+
+function Get-MrParameterCount {
+    param (
+        [string[]]$ParameterName
+    )
+
+    foreach ($Parameter in $ParameterName) {
+        $Results = Get-Command -ParameterName $Parameter -ErrorAction SilentlyContinue
+
+        [pscustomobject]@{
+            ParameterName = $Parameter
+            NumberOfCmdlets = $Results.Count
+        }
+    }
+}
+
+$x = 2; $y = 3
+Get-Power $x $y
+Get-PSVersion
+Get-MrParameterCount -ParameterName ComputerName, Computer, ServerName, Host, Machine
+
+# end of script
