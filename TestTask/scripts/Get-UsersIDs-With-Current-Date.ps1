0d67c6d445b9a8c15707ccf7d45cefb4e5ee3c18
Tania Haiuk <t.v.gayuk@gmail.com>
2022-01-24 21:02:07 +0200

    .\Get-UsersIDs-With-Current-Date.ps1 script added

---
+++ TestTask/scripts/Get-UsersIDs-With-Current-Date.ps1
@@ -0,0 +1,8 @@
+﻿function Get-Users-IDs-With-Current-Date([int[]]$users_ids) {
+    $ids_date = @{}
+
+    foreach ($id in $users_ids) {
+        $ids_date.Add($id, (Get-Date))
+    }
+    return $ids_date
+}
