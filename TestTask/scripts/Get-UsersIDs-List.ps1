6f16587c83837386419f7bbe59c0626eb44185b2
Tania Haiuk <t.v.gayuk@gmail.com>
2022-01-24 13:20:24 +0200

    Get-UsersIDs-List script created

---
+++ TestTask/scripts/Get-UsersIDs-List.ps1
@@ -0,0 +1,11 @@
+﻿$users = Get-Content -Path "..\resources\users.json" | ConvertFrom-JSON
+
+$active_users_list = @()
+
+foreach ($user in $users) {
+    if ($user.active -eq $true) {
+        $active_users_list += $user.id
+    }
+}
+
+return $active_users_list
